231df83ea10d530f46e79b7c3df23ac2dc75e254
Matheus Faria <mths.faria@outlook.com>
2022-11-08 12:49:44 -0300

    Fix errors

--- configure.sh
+++ configure.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# LightColor='\033[1;32m'
+# printf "I oaksofaoskfoasf${LightColor}love${NC} Stack Overflow\n"
+
+# Get current regular user (not sudo user)
+RUID=$(who | awk 'FNR == 1 {print $1}')
+RUSER_UID=$(id -u ${RUID})
+
 # Update + Upgrade
 apt update && apt upgrade -y
 
@@ -19,22 +26,26 @@ pip3 install pylint
 flatpak install -y --noninteractive flathub com.github.calo001.fondo
 flatpak install -y --noninteractive com.github.donadigo.appeditor
 
-# Install Chrome + Bitwarden
+# Install Chrome
 wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O google-chrome.deb
 dpkg -i google-chrome.deb
+apt install -fy
 
 # Install - Adapta Nokto Fonts
 wget "https://fonts.google.com/download?family=Roboto" -O roboto.zip
 wget "https://fonts.google.com/download?family=Noto Sans" -O noto_sans.zip
-
 unzip roboto.zip -d Roboto
 unzip noto_sans.zip -d Noto_Sans
-
 mv Roboto Noto_Sans /usr/share/fonts/
 
 # Install - Adapta Nokto theme
-wget "https://launchpadlibrarian.net/391325176/adapta-gtk-theme_3.95.0.11-0ubuntu1~bionic1_all.deb" -O adapta-gtk-theme.deb
-dpkg -i adapta-gtk-theme.deb
+wget "https://launchpadlibrarian.net/391325176/adapta-gtk-theme_3.95.0.11-0ubuntu1~bionic1_all.deb" -O adapta-nokto-gtk-theme.deb
+dpkg -i adapta-nokto-gtk-theme.deb
+apt install -fy
+mv -f cinnamon.css /usr/share/themes/Adapta-Nokto/cinnamon/cinnamon.css # Set Adapta Nokto panel color to black
+
+# Set virtualbox to use dark theme
+mv -f virtualbox.desktop /usr/share/applications/virtualbox.desktop
 
 # Install Sweet Theme
 tar -xf Sweet-mars-v40.tar.xz
@@ -44,55 +55,49 @@ mv Sweet-mars-v40 /usr/share/themes/
 git clone https://github.com/keeferrourke/la-capitaine-icon-theme /usr/share/icons/la-capitaine
 
 # ZSH and Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-chsh -s $(which zsh);
+sudo -u ${RUID} sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
-# Disable Recent Files
+# Disable; Recent 
 rm ~/.local/share/recently-used.xbel
 touch ~/.local/share/recently-used.xbel
-sudo chattr +i ~/.local/share/recently-used.xbel
+chattr +i ~/.local/share/recently-used.xbel
 
 # WPS Office Fonts
 git clone https://github.com/udoyen/wps-fonts.git /tmp/wps-fonts
 mv /tmp/wps-fonts/wps /usr/share/fonts/
 
 # Install ASDF
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+sudo -u ${RUID} git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
 # Install Teamviewer
 wget "https://download.teamviewer.com/download/linux/teamviewer_amd64.deb" -O teamviewer.deb
 dpkg -i teamviewer.deb
+apt install -fy
 
 # Install Dropbox
 wget "https://linux.dropbox.com/packages/ubuntu/dropbox_2020.03.04_amd64.deb" -O dropbox.deb
 dpkg -i dropbox.deb
-
-# Fix apt configure
-apt install -f -y
+apt install -fy
 
 # Install Vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 apt update
 rm -f packages.microsoft.gpg
 apt install code -y
 
-# -------------------------
 # Set themes and wallpaper
-RUID=$(who | awk 'FNR == 1 {print $1}')
-RUSER_UID=$(id -u ${RUID})
 sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.background picture-uri "file:///$PWD/wallpaper.jpg"
-# sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.theme name 'Adapta-Nokto' # set área de trabalho
-# sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface icon-theme 'la-capitaine' # set icons
-# sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface gtk-theme 'Sweet-mars-v40' # set aplicativos
-# sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface cursor-theme 'Adwaita' # set cursor theme
 
 # Home folder - config files
 git clone https://github.com/quantux/home /tmp/home
-rsync -av /tmp/home/* /home/${RUID}/.
+rsync -av /tmp/home/ /home/${RUID}/
 
 # Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sudo -u ${RUID} curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo -u ${RUID} sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sudo -u ${RUID} vim -c :PlugInstall -c :q -c :q
 
+# Load dconf file
+sudo -u ${RUID} dconf load / < dconf_cinnamon_settings
